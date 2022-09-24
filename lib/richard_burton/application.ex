99c276b31778df645e1a87a3d279d08a4939d5a7
Andrés Vidal <andres@wyeworks.com>
2022-09-24 17:31:28 -0300

    Initial configuration

---
+++ lib/richard_burton/application.ex
@@ -0,0 +1,36 @@
+defmodule RichardBurton.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      RichardBurton.Repo,
+      # Start the Telemetry supervisor
+      RichardBurtonWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: RichardBurton.PubSub},
+      # Start the Endpoint (http/https)
+      RichardBurtonWeb.Endpoint
+      # Start a worker by calling: RichardBurton.Worker.start_link(arg)
+      # {RichardBurton.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: RichardBurton.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    RichardBurtonWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/richard_burton/repo.ex
@@ -0,0 +1,5 @@
+defmodule RichardBurton.Repo do
+  use Ecto.Repo,
+    otp_app: :richard_burton,
+    adapter: Ecto.Adapters.Postgres
+end
