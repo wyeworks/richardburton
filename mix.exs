bc6ff50d8c5e588eb91c594b4b5614241d02b0c0
Andrés Vidal <andres@wyeworks.com>
2022-09-25 14:06:04 -0300

    Add mix task to load data from csv

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule RichardBurton.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:csv, "~> 2.5.0"}
     ]
   end
 
