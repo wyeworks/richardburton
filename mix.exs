d93361d65a1ea49bb5ef5ab4b2cecb8ae1506eb1
Andrés Vidal <andres@wyeworks.com>
2022-09-25 12:16:32 -0300

    Install credo

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule RichardBurton.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
