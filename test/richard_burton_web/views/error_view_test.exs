50516a2b06fec8d25c55e338b9766b8ae2b0d6a5
Andrés Vidal <andres@wyeworks.com>
2022-09-26 17:32:56 -0300

    Run formatter

--- test/richard_burton_web/views/error_view_test.exs
+++ test/richard_burton_web/views/error_view_test.exs
@@ -5,7 +5,9 @@ defmodule RichardBurtonWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(RichardBurtonWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(RichardBurtonWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500.json" do
