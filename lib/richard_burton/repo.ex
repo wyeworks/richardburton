5d2f74cd76a1ef7acdcc369b48a0018afb879270
Andrés Vidal <andres@wyeworks.com>
2022-09-27 01:13:12 -0300

    Add Publication schema

--- lib/richard_burton/repo.ex
+++ lib/richard_burton/repo.ex
@@ -6,11 +6,30 @@ defmodule RichardBurton.Repo do
   def maybe_insert(changeset, keys) do
     %module{} = changeset.data
 
+    keyword_list_id =
+      changeset.changes
+      |> Enum.filter(fn {key, _val} -> Enum.member?(keys, key) end)
+      |> Enum.map(&parse_keyword_list_id/1)
+
     changeset
     |> insert()
     |> case do
       {:ok, struct} -> struct
-      {:error, _} -> get_by!(module, keys)
+      {:error, _} -> get_by!(module, keyword_list_id)
     end
+  end
+
+  defp parse_keyword_list_id({key, %Ecto.Changeset{} = changeset}) do
+    parsed_key =
+      key
+      |> Atom.to_string()
+      |> Kernel.<>("_id")
+      |> String.to_existing_atom()
+
+    {parsed_key, changeset.data.id}
+  end
+
+  defp parse_keyword_list_id({key, val}) do
+    {key, val}
   end
 end
