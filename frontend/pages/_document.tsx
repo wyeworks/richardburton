e39921948b366b5ebb9d2ca14bd29b7d2cf039e1
Andrés Vidal <andres@wyeworks.com>
2022-09-28 17:18:20 -0300

    Initialize frontend project

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { DocumentProps, Html, Head, Main, NextScript } from "next/document";
+import { FC } from "react";
+
+const Document: FC<DocumentProps> = () => {
+  return (
+    <Html className="h-screen">
+      <Head>
+        <title>Richard Burton</title>
+      </Head>
+      <body className="h-screen font-light text-gray-900 bg-gray-100 font-montserrat">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
