eb0e13804171c485543af875f40e74707f3177d3
Andrés Vidal <andres@wyeworks.com>
2022-09-30 13:16:42 -0300

    Position column toggles

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,12 +9,6 @@ import {
   TranslatedBook,
 } from "types";
 import PublicationIndex from "components/PublicationIndex";
-import {
-  autoUpdate,
-  flip,
-  offset,
-  useFloating,
-} from "@floating-ui/react-dom-interactions";
 import Toggle from "components/Toggle";
 import Head from "next/head";
 import { useState } from "react";
@@ -57,12 +51,6 @@ const Home: NextPage = () => {
     }
   );
 
-  const { reference, floating, x, y, strategy } = useFloating({
-    placement: "right-start",
-    middleware: [offset(20), flip()],
-    whileElementsMounted: autoUpdate,
-  });
-
   const [columns, setColumns] = useState(new Set(DEFAULT_COLUMNS));
 
   return (
@@ -70,7 +58,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Richard Burton</title>
       </Head>
-      <div className="flex flex-col h-full p-8 overflow-hidden gap-y-8 2xl:mx-auto max-w-7xl pr-52">
+      <div className="relative flex flex-col h-full p-8 overflow-hidden gap-y-8 2xl:mx-auto max-w-7xl pr-52">
         <header className="space-y-2 text-center ">
           <h1 className="text-5xl">Richard Burton Platform</h1>
           <h2 className="text-2xl">
@@ -78,40 +66,38 @@ const Home: NextPage = () => {
           </h2>
         </header>
 
-        <aside
-          ref={floating}
-          style={{ top: y ?? 0, left: x ?? 0, position: strategy }}
-          className="w-48 p-2 space-y-2 rounded"
-        >
-          {PUBLICATION_ATTRIBUTES.map((attribute) => {
-            const isActive = columns.has(attribute);
-            const newColumns = new Set(columns);
+        <main className="w-full overflow-scroll">
+          <>
+            <aside className="absolute right-0 w-48 p-2 space-y-2">
+              {PUBLICATION_ATTRIBUTES.map((attribute) => {
+                const isActive = columns.has(attribute);
+                const newColumns = new Set(columns);
 
-            if (isActive) {
-              newColumns.delete(attribute);
-            } else {
-              newColumns.add(attribute);
-            }
+                if (isActive) {
+                  newColumns.delete(attribute);
+                } else {
+                  newColumns.add(attribute);
+                }
 
-            return (
-              <Toggle
-                key={attribute}
-                label={PUBLICATION_ATTRIBUTE_LABELS[attribute]}
-                startsChecked={isActive}
-                onChange={() => setColumns(newColumns)}
+                return (
+                  <Toggle
+                    key={attribute}
+                    label={PUBLICATION_ATTRIBUTE_LABELS[attribute]}
+                    startsChecked={isActive}
+                    onChange={() => setColumns(newColumns)}
+                  />
+                );
+              })}
+            </aside>
+            {translatedBooks ? (
+              <PublicationIndex
+                entries={toFlatPublications(translatedBooks)}
+                columns={columns}
               />
-            );
-          })}
-        </aside>
-        <main ref={reference} className="w-full overflow-scroll">
-          {translatedBooks ? (
-            <PublicationIndex
-              entries={toFlatPublications(translatedBooks)}
-              columns={columns}
-            />
-          ) : (
-            "loading..."
-          )}
+            ) : (
+              "loading..."
+            )}
+          </>
         </main>
       </div>
     </>
