5fcb64b40049a8a9fef5959390ffce6b29be9a50
Andrés Vidal <andres@wyeworks.com>
2022-10-02 19:42:59 -0300

    Parse and store uploaded csv file

--- frontend/components/PublicationUpload.tsx
+++ frontend/components/PublicationUpload.tsx
@@ -1,12 +1,51 @@
-import { FC } from "react";
+import { ChangeEventHandler, FC, useState } from "react";
 import UploadIcon from "assets/upload.svg";
+import { FlatPublication } from "types";
 
 const PublicationUpload: FC = () => {
+  const [, setUploadedPublications] = useState<FlatPublication[] | undefined>();
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = async (event) => {
+    if (event.target.files) {
+      const [file] = event.target.files;
+
+      setUploadedPublications(
+        (await file.text())
+          .split("\n")
+          .map((row) => row.split(";"))
+          .map(
+            ([
+              originalAuthors,
+              year,
+              country,
+              originalTitle,
+              title,
+              authors,
+              publisher,
+            ]) => ({
+              originalAuthors,
+              year: parseInt(year),
+              country,
+              originalTitle,
+              title,
+              authors,
+              publisher,
+            })
+          )
+      );
+    }
+  };
+
   return (
     <label className="flex flex-col items-center justify-center h-40 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200">
       Upload .csv
       <UploadIcon className="w-8 h-8 text-indigo-800" />
-      <input type="file" id="upload-csv" className="hidden" />
+      <input
+        type="file"
+        id="upload-csv"
+        className="hidden"
+        onChange={handleChange}
+      />
     </label>
   );
 };
