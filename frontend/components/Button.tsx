81dba1bfe979c25dcb7f560af7e0341052442cc2
Andrés Vidal <andres@wyeworks.com>
2022-10-04 12:51:55 -0300

    Make Button consistent to Toggle styles

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -5,7 +5,7 @@ type Props = { label: string; onClick: () => void };
 const Button: FC<Props> = ({ label, onClick }) => {
   return (
     <button
-      className="w-48 p-2 text-xl font-medium text-white transition-colors bg-indigo-600 rounded-lg shadow-sm font-base hover:bg-indigo-700"
+      className="w-48 p-1 text-white transition-colors bg-indigo-600 rounded-lg shadow-sm font-base hover:bg-indigo-700"
       onClick={onClick}
     >
       {label}
