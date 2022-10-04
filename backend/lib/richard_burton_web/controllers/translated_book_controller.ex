a6364658c445c2582c7a1c7f1a6a9e75132ab8fe
Andrés Vidal <andres@wyeworks.com>
2022-10-03 22:57:31 -0300

    The API exposes publications instead of translated books

--- backend/lib/richard_burton_web/controllers/translated_book_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule RichardBurtonWeb.TranslatedBookController do
-  use RichardBurtonWeb, :controller
-
-  alias RichardBurton.TranslatedBook
-
-  def index(conn, _params) do
-    json(conn, TranslatedBook.all())
-  end
-end
