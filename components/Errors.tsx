6774ca52fba65596e1ff3eb334ffa474c2a41cea
Andrés Vidal <andres@wyeworks.com>
2022-12-15 13:17:11 -0300

    Refactor snackbar rendering

--- components/Errors.tsx
+++ components/Errors.tsx
@@ -35,37 +35,38 @@ const Errors: FC = () => {
   useEffect(() => resetErrors, []);
 
   const shownErrorCount =
-    errors.length === errors.length
-      ? MAX_SNACKBARS - 1
-      : Math.min(MAX_SNACKBARS, errors.length);
+    errors.length === MAX_SNACKBARS
+      ? errors.length
+      : Math.min(MAX_SNACKBARS - 1, errors.length);
 
-  const stackedErrors = errors.slice(shownErrorCount);
-  const shownErrors = errors.slice(0, shownErrorCount);
+  const stackedErrorCount = errors.length - shownErrorCount;
+
+  const snackbars = errors
+    .slice(0, shownErrorCount)
+    .map(({ message, id }) => ({ message, key: id }))
+    .concat({
+      message: `${stackedErrorCount} more errors`,
+      key: "error-stack",
+    });
 
   return (
     <section className="fixed z-50 flex flex-col-reverse items-center space-y-2 space-y-reverse -translate-x-1/2 left-1/2 top-10">
       <AnimatePresence>
-        {shownErrors.map(({ id, message }) => (
-          <motion.div
-            layout
-            key={id}
-            className="px-3 py-2 space-x-2 bg-white rounded shadow-md w-96"
-            initial={{ scale: 0.9, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.9, opacity: 0 }}
-            transition={{ duration: 0.1 }}
-          >
-            <span>⚠️</span> <span>{message}</span>
-          </motion.div>
-        ))}
-        {stackedErrors.length > 0 && (
-          <motion.div
-            layout
-            key="error-stack"
-            className="px-3 py-2 space-x-2 bg-white rounded shadow-md w-96"
-          >
-            <span>⚠️</span> <span>{stackedErrors.length} more errors</span>
-          </motion.div>
+        {snackbars.map(
+          ({ key, message }) =>
+            (key !== "error-stack" || stackedErrorCount > 0) && (
+              <motion.div
+                layout
+                key={key}
+                className="px-3 py-2 space-x-2 bg-white rounded shadow-md w-96"
+                initial={{ scale: 0.9, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.9, opacity: 0 }}
+                transition={{ duration: 0.1 }}
+              >
+                <span>⚠️</span> <span>{message}</span>
+              </motion.div>
+            )
         )}
       </AnimatePresence>
     </section>
