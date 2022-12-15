e889ed4a31187c5ee40f835d7bf0383ef4c9434d
Andrés Vidal <andres@wyeworks.com>
2022-12-15 17:43:44 -0300

    Implement unpolished file upload

--- backend/lib/richard_burton_web/controllers/publication_controller.ex
+++ backend/lib/richard_burton_web/controllers/publication_controller.ex
@@ -24,4 +24,43 @@ defmodule RichardBurtonWeb.PublicationController do
 
     conn |> put_status(status) |> json(response_body)
   end
+
+  def create_all(conn, %{"csv" => %Plug.Upload{path: path}}) do
+    publications =
+      path
+      |> File.stream!()
+      |> CSV.decode!(
+        separator: ?;,
+        headers: [
+          :original_authors,
+          :year,
+          :country,
+          :original_title,
+          :title,
+          :authors,
+          :publisher
+        ]
+      )
+      |> Enum.map(&deserialize/1)
+
+    conn |> put_status(:ok) |> json(publications)
+  end
+
+  defp deserialize(row) do
+    {year, _} = Integer.parse(row.year)
+
+    %{
+      "title" => row.title,
+      "year" => year,
+      "country" => row.country,
+      "publisher" => row.publisher,
+      "translated_book" => %{
+        "authors" => row.authors,
+        "original_book" => %{
+          "title" => row.original_title,
+          "authors" => row.original_authors
+        }
+      }
+    }
+  end
 end
