029c79a5a5b79b682ef7e293acfe6c39aa935d63
Andrés Vidal <andres@wyeworks.com>
2022-12-25 22:05:36 -0300

    Refactor PublicationCodecTest

--- backend/test/richard_burton/publication_codec_test.exs
+++ backend/test/richard_burton/publication_codec_test.exs
@@ -11,81 +11,80 @@ defmodule RichardBurton.PublicationCodecTest do
   alias RichardBurton.OriginalBook
 
   describe "from_csv!/1 when the provided csv is valid" do
+    @output [
+      %{
+        "title" => "Iraçéma the Honey-Lips: A Legend of Brazil",
+        "year" => "1886",
+        "country" => "GB",
+        "publisher" => "Bickers & Son",
+        "translated_book" => %{
+          "authors" => "Isabel Burton",
+          "original_book" => %{
+            "authors" => "José de Alencar",
+            "title" => "Iracema"
+          }
+        }
+      },
+      %{
+        "title" => "Ubirajara: A Legend of the Tupy Indians",
+        "year" => "1922",
+        "country" => "US",
+        "publisher" => "Ronald Massey",
+        "translated_book" => %{
+          "authors" => "J. T. W. Sadler",
+          "original_book" => %{
+            "authors" => "José de Alencar",
+            "title" => "Ubirajara"
+          }
+        }
+      },
+      %{
+        "title" => "",
+        "year" => "1886",
+        "country" => "GB",
+        "publisher" => "Bickers & Son",
+        "translated_book" => %{
+          "authors" => "",
+          "original_book" => %{
+            "authors" => "José de Alencar",
+            "title" => "Iracema"
+          }
+        }
+      },
+      %{
+        "title" => "Ubirajara: A Legend of the Tupy Indians",
+        "year" => "",
+        "country" => "",
+        "publisher" => "",
+        "translated_book" => %{
+          "authors" => "J. T. W. Sadler",
+          "original_book" => %{
+            "authors" => "",
+            "title" => ""
+          }
+        }
+      }
+    ]
+
     test "when the provided csv file is valid, returns a list of publication-like maps" do
-      assert [
-               %{
-                 "title" => "Iraçéma the Honey-Lips: A Legend of Brazil",
-                 "year" => "1886",
-                 "country" => "GB",
-                 "publisher" => "Bickers & Son",
-                 "translated_book" => %{
-                   "authors" => "Isabel Burton",
-                   "original_book" => %{
-                     "authors" => "José de Alencar",
-                     "title" => "Iracema"
-                   }
-                 }
-               },
-               %{
-                 "title" => "Ubirajara: A Legend of the Tupy Indians",
-                 "year" => "1922",
-                 "country" => "US",
-                 "publisher" => "Ronald Massey",
-                 "translated_book" => %{
-                   "authors" => "J. T. W. Sadler",
-                   "original_book" => %{
-                     "authors" => "José de Alencar",
-                     "title" => "Ubirajara"
-                   }
-                 }
-               },
-               %{
-                 "title" => "",
-                 "year" => "1886",
-                 "country" => "GB",
-                 "publisher" => "Bickers & Son",
-                 "translated_book" => %{
-                   "authors" => "",
-                   "original_book" => %{
-                     "authors" => "José de Alencar",
-                     "title" => "Iracema"
-                   }
-                 }
-               },
-               %{
-                 "title" => "Ubirajara: A Legend of the Tupy Indians",
-                 "year" => "",
-                 "country" => "",
-                 "publisher" => "",
-                 "translated_book" => %{
-                   "authors" => "J. T. W. Sadler",
-                   "original_book" => %{
-                     "authors" => "",
-                     "title" => ""
-                   }
-                 }
-               }
-             ] = PublicationCodec.from_csv!("test/fixtures/data_valid_mixed.csv")
+      assert @output == PublicationCodec.from_csv!("test/fixtures/data_valid_mixed.csv")
     end
   end
 
   describe "from_csv!/1 when the provided csv is invalid" do
     test "because it has an incorrect format, raise an error" do
-      assert_raise RuntimeError, fn ->
-        PublicationCodec.from_csv!("test/fixtures/data_invalid_format.csv")
-      end
+      input = "test/fixtures/data_invalid_format.csv"
+      assert_raise RuntimeError, fn -> PublicationCodec.from_csv!(input) end
     end
 
     test "because it has an invalid separator, raise an error" do
-      assert_raise CSV.RowLengthError, fn ->
-        PublicationCodec.from_csv!("test/fixtures/data_invalid_separator.csv")
-      end
+      input = "test/fixtures/data_invalid_separator.csv"
+      assert_raise CSV.RowLengthError, fn -> PublicationCodec.from_csv!(input) end
     end
 
     test "because it is incomplete, raise an error" do
-      assert_raise CSV.RowLengthError, fn ->
-        PublicationCodec.from_csv!("test/fixtures/data_invalid_incomplete.csv")
-      end
+      input = "test/fixtures/data_invalid_incomplete.csv"
+      assert_raise CSV.RowLengthError, fn -> PublicationCodec.from_csv!(input) end
     end
   end
 
