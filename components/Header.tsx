ae82214f5dfaf8506a0b3a859ea817a2cd21454b
Andrés Vidal <andres@wyeworks.com>
2022-12-27 02:11:17 -0300

    Make Header variants more alike

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,22 +3,25 @@ import Link from "next/link";
 
 type Props = { compact?: boolean };
 
+const HEADING_TEXT = "Richard Burton Platform";
+const SUBHEADING_TEXT = " A database about Brazilian literature in translation";
+
 const Header: FC<Props> = ({ compact }) => {
   return compact ? (
     <header className="py-2 text-center text-white bg-indigo-600">
-      <Link href="/" className="inline px-4 py-1 text-xl font-medium border-r">
-        Richard Burton Platform
-      </Link>
-      <span className="inline px-4 text-lg">
-        A database about Brazilian literature in translation
-      </span>
+      <h1>
+        <Link href="/" className="px-4 py-1 text-xl font-medium border-r ">
+          {HEADING_TEXT}
+        </Link>
+        <span className="px-4 text-lg">{SUBHEADING_TEXT}</span>
+      </h1>
     </header>
   ) : (
     <header className="my-4 text-center">
-      <h1 className="text-5xl">Richard Burton Platform</h1>
-      <h2 className="text-2xl">
-        A database about Brazilian literature in translation
-      </h2>
+      <h1 className="text-5xl">
+        <Link href="/">{HEADING_TEXT}</Link>
+        <div className="text-2xl tracking-wide">{SUBHEADING_TEXT}</div>
+      </h1>
     </header>
   );
 };
