7f601fae8a3ba062ffb9160914f221223862d2b4
Andrés Vidal <andres@wyeworks.com>
2022-12-26 21:09:40 -0300

    Tweak sidebar styling

--- frontend/components/PublicationUpload.tsx
+++ frontend/components/PublicationUpload.tsx
@@ -56,7 +56,7 @@ const PublicationUpload: FC = () => {
   useEffect(() => resetPublications(), [resetPublications]);
 
   return (
-    <label className="flex flex-col items-center justify-center h-40 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200">
+    <label className="flex flex-col items-center justify-center w-40 h-40 text-sm rounded-lg shadow-sm cursor-pointer hover:bg-gray-200">
       Upload .csv
       <UploadIcon className="w-8 h-8 text-indigo-800" />
       <input
