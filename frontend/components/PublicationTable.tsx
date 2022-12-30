454e5276d44501330682e729a84fa0bb06358298
Andrés Vidal <andres@wyeworks.com>
2022-12-30 16:27:41 -0300

    PublicationIndex now extends PublicationTable

--- frontend/components/PublicationTable.tsx
+++ frontend/components/PublicationTable.tsx
@@ -4,62 +4,148 @@ import {
   PublicationId,
   PublicationKey,
 } from "modules/publications";
-import { FC } from "react";
+import { FC, MouseEvent, ReactNode } from "react";
 
-const ColumnHeader: FC<{ attribute: PublicationKey }> = ({ attribute }) => {
-  const isVisible = Publication.STORE.ATTRIBUTES.useIsVisible(attribute);
+type RowId = PublicationId;
+type ColId = PublicationKey;
+
+const ColumnHeader: FC<{ colId: ColId }> = ({ colId }) => {
+  const isVisible = Publication.STORE.ATTRIBUTES.useIsVisible(colId);
 
   return isVisible ? (
-    <th className="px-2 py-4">{Publication.ATTRIBUTE_LABELS[attribute]}</th>
+    <th className="px-2 py-4">{Publication.ATTRIBUTE_LABELS[colId]}</th>
   ) : null;
 };
 
-const Column: FC<{
-  publicationId: PublicationId;
-  attribute: PublicationKey;
-}> = ({ publicationId, attribute }) => {
-  const isVisible = Publication.STORE.ATTRIBUTES.useIsVisible(attribute);
-  const content = Publication.STORE.ATTRIBUTES.useValue(
-    publicationId,
-    attribute
-  );
+const Content: FC<{
+  rowId: RowId;
+  colId: ColId;
+  error: string;
+  value: string | number;
+}> = ({ value }) => {
+  return <div className="px-2 py-1 truncate">{value}</div>;
+};
 
-  return isVisible ? (
-    <td className={"max-w-xs px-2 py-1 justify group-hover:bg-indigo-100"}>
-      <div className="px-2 py-1 truncate">{content}</div>
+const Column: FC<{
+  rowId: RowId;
+  colId: ColId;
+  Content: typeof Content;
+  visible?: boolean;
+  invalid?: boolean;
+  selected?: boolean;
+  selectable?: boolean;
+}> = ({
+  rowId,
+  colId,
+  Content,
+  invalid = false,
+  selected = false,
+  selectable = false,
+}) => {
+  const { useIsVisible, useValue, useErrorDescription } =
+    Publication.STORE.ATTRIBUTES;
+
+  const visible = useIsVisible(colId);
+  const value = useValue(rowId, colId);
+  const error = useErrorDescription(rowId, colId);
+
+  return visible ? (
+    <td
+      className="max-w-xs px-2 py-1 justify group-hover:bg-indigo-100 error:group-hover:bg-red-100 selected:bg-amber-100"
+      data-selected={selected}
+      data-selectable={selectable}
+      data-error={invalid}
+    >
+      <Content rowId={rowId} colId={colId} value={value} error={error} />
     </td>
   ) : null;
 };
 
-const Row: FC<{ publicationId: PublicationId }> = ({ publicationId }) => {
+const Row: FC<{
+  id: RowId;
+  Column: typeof Column;
+  Content: typeof Content;
+  SignalColumn?: typeof SignalColumn;
+  onClick?: (event: MouseEvent) => void;
+}> = ({ id, Column, Content, SignalColumn, onClick }) => {
+  const clickable = Boolean(onClick);
   return (
-    <tr className="relative group" data-selectable="true">
+    <tr
+      className={classNames("relative group", clickable && "cursor-pointer")}
+      onClick={onClick}
+    >
+      {SignalColumn && <SignalColumn rowId={id} />}
       {Publication.ATTRIBUTES.map((attribute) => (
         <Column
           key={attribute}
-          attribute={attribute}
-          publicationId={publicationId}
+          colId={attribute}
+          rowId={id}
+          Content={Content}
         />
       ))}
     </tr>
   );
 };
 
-const PublicationTable: FC = () => {
+const SignalColumn: FC<{
+  rowId: RowId;
+  invalid?: boolean;
+  selected?: boolean;
+  selectable?: boolean;
+  children?: ReactNode;
+}> = ({ invalid = false, selected = false, selectable = false, children }) => {
+  return (
+    <td
+      className="sticky left-0 px-2 text-xl bg-gray-100 group-hover:bg-indigo-100 error:group-hover:bg-red-100 selected:bg-amber-100"
+      data-selected={selected}
+      data-selectable={selectable}
+      data-error={invalid}
+    >
+      {children}
+    </td>
+  );
+};
+
+type Props = {
+  ExtendedRow?: typeof Row;
+  ExtendedColumn?: typeof Column;
+  ExtendedContent?: typeof Content;
+  ExtendedSignalColumn?: typeof SignalColumn;
+
+  onRowClick?: (id: RowId) => (event: MouseEvent) => void;
+  className?: string;
+};
+
+const PublicationTable: FC<Props> = ({
+  ExtendedRow = Row,
+  ExtendedColumn = Column,
+  ExtendedContent = Content,
+  ExtendedSignalColumn,
+  onRowClick,
+  className,
+}) => {
   const ids = Publication.STORE.useVisibleIds();
 
   return (
-    <table className={classNames("overflow-auto")}>
+    <table className={classNames(className, "overflow-auto")}>
       <thead className="sticky top-0 z-10 bg-gray-100">
         <tr>
+          {ExtendedSignalColumn && <th />}
           {Publication.ATTRIBUTES.map((key) => (
-            <ColumnHeader key={key} attribute={key} />
+            <ColumnHeader key={key} colId={key} />
           ))}
         </tr>
       </thead>
       <tbody>
         {ids.map((id) => (
-          <Row key={id} publicationId={id} />
+          <ExtendedRow
+            key={id}
+            id={id}
+            Column={ExtendedColumn}
+            SignalColumn={ExtendedSignalColumn}
+            Content={ExtendedContent}
+            onClick={onRowClick?.(id)}
+          />
         ))}
       </tbody>
     </table>
@@ -67,3 +153,5 @@ const PublicationTable: FC = () => {
 };
 
 export default PublicationTable;
+export type { RowId };
+export { Row, Column, Content, SignalColumn };
