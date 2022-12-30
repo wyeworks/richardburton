30e095e1cbd03015f3bfe661e8c0da076f95bca3
Andrés Vidal <andres@wyeworks.com>
2022-12-30 16:44:19 -0300

    Fix error tooltips

--- frontend/components/ErrorTooltip.tsx
+++ frontend/components/ErrorTooltip.tsx
@@ -4,16 +4,14 @@ import Tooltip, { TooltipProps } from "./Tooltip";
 
 type Props = Omit<TooltipProps, "content"> & {
   message: string;
-  hidden: boolean;
 };
 
-const ErrorTooltip: FC<Props> = ({ children, message, hidden, ...props }) => {
+const ErrorTooltip: FC<Props> = ({ children, message, ...props }) => {
   const content = (
     <div
-      className={classNames(
-        "flex items-center pl-2 pr-3 py-1.5 space-x-1 text-white bg-red-500 rounded shadow-md",
-        { hidden }
-      )}
+      className={
+        "flex items-center pl-2 pr-3 py-1.5 space-x-1 text-white bg-red-500 rounded shadow-md"
+      }
     >
       <span role="presentation" className="text-xl">
         ❕
@@ -22,10 +20,12 @@ const ErrorTooltip: FC<Props> = ({ children, message, hidden, ...props }) => {
     </div>
   );
 
-  return (
+  return Boolean(message) ? (
     <Tooltip {...props} content={content}>
       {children}
     </Tooltip>
+  ) : (
+    <>{children}</>
   );
 };
 
