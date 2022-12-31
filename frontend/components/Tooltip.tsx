381f4caa44b51a060df4d7e2e9f3642075078dd6
Andrés Vidal <andres@wyeworks.com>
2022-12-31 17:03:51 -0300

    Enhance feedback to user

--- frontend/components/Tooltip.tsx
+++ frontend/components/Tooltip.tsx
@@ -6,6 +6,7 @@ type Props = Omit<TooltipProps, "content"> & {
   message: string;
   error?: boolean;
   warning?: boolean;
+  info?: boolean;
 };
 
 const Tooltip: FC<Props> = ({
@@ -13,6 +14,7 @@ const Tooltip: FC<Props> = ({
   message,
   error,
   warning,
+  info,
   ...props
 }) => {
   const content = (
@@ -21,12 +23,20 @@ const Tooltip: FC<Props> = ({
         "flex items-center py-1 text-sm space-x-1 m-2 rounded shadow",
         {
           "text-white bg-red-500 pl-1 pr-2": error,
-          "bg-white px-2 shadow": warning,
+          "bg-white pl-1 pr-2 shadow": warning || info,
         }
       )}
     >
-      <span role="presentation" className="self-start text-base">
-        {error ? "❕" : "⚠️"}
+      <span
+        role="presentation"
+        className={classNames("self-start", {
+          "text-base": warning || error,
+          "text-indigo-700 text-xl mb-0.5": info,
+        })}
+      >
+        {error && "❕"}
+        {warning && "⚠️"}
+        {info && "ℹ"}
       </span>
 
       <span>{message}</span>
