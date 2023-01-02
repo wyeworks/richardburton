a2eebcd85125bf2cfb24d0c0709e7b7b7b6d0a28
Andrés Vidal <andres@wyeworks.com>
2023-01-02 18:12:02 -0300

    Link TranslatedBook to Author

--- backend/lib/richard_burton/translated_book.ex
+++ backend/lib/richard_burton/translated_book.ex
@@ -5,18 +5,24 @@ defmodule RichardBurton.TranslatedBook do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias RichardBurton.Author
   alias RichardBurton.Repo
   alias RichardBurton.OriginalBook
+  alias RichardBurton.TranslatedBook
   alias RichardBurton.Publication
+  alias RichardBurton.Util
 
   @external_attributes [:authors, :original_book]
 
   @derive {Jason.Encoder, only: @external_attributes}
   schema "translated_books" do
-    field(:authors, :string)
+    field(:authors_fingerprint, :string)
+
+    has_many(:publications, Publication)
 
     belongs_to(:original_book, OriginalBook)
-    has_many(:publications, Publication)
+
+    many_to_many(:authors, Author, join_through: "translated_book_authors")
 
     timestamps()
   end
@@ -26,44 +32,61 @@ defmodule RichardBurton.TranslatedBook do
     # Compute basic changeset with original_book validation
     result =
       translated_book
-      |> cast(attrs, [:authors])
-      |> cast_assoc(:original_book)
-      |> validate_required([:authors, :original_book])
+      |> cast(attrs, [])
+      |> cast_assoc(:authors, required: true)
+      |> cast_assoc(:original_book, required: true)
+      |> validate_length(:authors, min: 1)
 
     # Check if original_book is valid
     if result.valid? do
       # Insert or fetch the valid original book
-      original_book = OriginalBook.maybe_insert!(attrs["original_book"])
+      original_book_attrs = result |> get_field(:original_book) |> OriginalBook.to_map()
+      original_book = OriginalBook.maybe_insert!(original_book_attrs)
 
-      # Compute complete changeset with the complete original_book associated
+      # Insert or fetch the valid authors
+      authors_attrs = result |> get_field(:authors) |> Enum.map(&Author.to_map/1)
+      authors = Enum.map(authors_attrs, &Author.maybe_insert!/1)
+
+      authors_fingerprint =
+        authors_attrs
+        |> Enum.map_join(&Map.get(&1, "name"))
+        |> Util.create_fingerprint()
+
+      # Compute complete changeset with the complete original book and authors associated
       result
+      |> put_change(:authors_fingerprint, authors_fingerprint)
+      |> put_assoc(:authors, authors)
       |> put_assoc(:original_book, original_book)
-      |> unique_constraint([:authors, :original_book_id])
+      |> unique_constraint([:authors_fingerprint, :original_book_id])
     else
       # Return the changeset with the original_book validation errors
       result
     end
   end
 
-  @spec maybe_insert!(
-          :invalid
-          | %{optional(:__struct__) => none, optional(atom | binary) => any}
-        ) :: any
   def maybe_insert!(attrs) do
-    %__MODULE__{}
+    %TranslatedBook{}
     |> changeset(attrs)
-    |> Repo.maybe_insert!([:authors, :original_book])
+    |> Repo.maybe_insert!([:authors_fingerprint, :original_book])
   end
 
   def all() do
-    __MODULE__ |> Repo.all() |> Repo.preload(:original_book)
+    TranslatedBook
+    |> Repo.all()
+    |> preload
+  end
+
+  def preload(data) do
+    Repo.preload(data, [:authors, original_book: [:authors]])
   end
 
   def to_map(translated_book) do
     original_book = OriginalBook.to_map(translated_book.original_book)
+    authors = Enum.map(translated_book.authors, &Author.to_map/1)
 
     translated_book
     |> Map.take(@external_attributes)
     |> Map.put(:original_book, original_book)
+    |> Map.put(:authors, authors)
   end
 end
--- backend/test/richard_burton/translated_book_test.exs
+++ backend/test/richard_burton/translated_book_test.exs
@@ -6,46 +6,118 @@ defmodule RichardBurton.TranslatedBookTest do
   use RichardBurton.DataCase
 
   alias RichardBurton.TranslatedBook
+  alias RichardBurton.Util
 
   @valid_attrs %{
-    "authors" => "Richard Burton and Isabel Burton",
+    "authors" => [
+      %{"name" => "Richard Burton"},
+      %{"name" => "Isabel Burton"}
+    ],
     "original_book" => %{
-      "authors" => "J. M. Pereira da Silva",
-      "title" => "Manuel de Moraes: crônica do século XVII"
+      "title" => "Manuel de Moraes: crônica do século XVII",
+      "authors" => [
+        %{"name" => "J. M. Pereira da Silva"}
+      ]
     }
   }
 
-  def entity do
-    {%TranslatedBook{}, &TranslatedBook.changeset/2, @valid_attrs}
+  @valid_attrs_atom_keys %{
+    authors: [
+      %{name: "Richard Burton"},
+      %{name: "Isabel Burton"}
+    ],
+    original_book: %{
+      title: "Manuel de Moraes: crônica do século XVII",
+      authors: [
+        %{name: "J. M. Pereira da Silva"}
+      ]
+    }
+  }
+
+  defp changeset(attrs = %{}) do
+    TranslatedBook.changeset(%TranslatedBook{}, attrs)
+  end
+
+  defp change_valid(attrs = %{}) do
+    changeset(Util.deep_merge_maps(@valid_attrs, attrs))
+  end
+
+  defp insert(attrs) do
+    attrs |> changeset() |> Repo.insert()
+  end
+
+  defp count do
+    Kernel.length(TranslatedBook.all())
   end
 
   describe "changeset/2" do
-    test "when valid attributes are provided, is valid", do: test_valid_attrs()
-    test "when authors is blank, is invalid", do: test_not_blank("authors")
-    test "when authors is nil, is invalid", do: test_not_nil("authors")
-
-    test "when original_book is empty, is invalid",
-      do: test_invalid_attr_value("original_book", %{})
-
-    test "when original_book is nil, is invalid",
-      do: test_not_nil("original_book")
-
-    test "when a translated_book with the provided attributes exists, is invalid",
-      do: test_unique_constraint(:authors)
-  end
-
-  describe("maybe_insert/1") do
-    test "when there is no translated_book with the provided authors and original_book, inserts it" do
-      assert [] = TranslatedBook.all()
-      translated_book = TranslatedBook.maybe_insert!(@valid_attrs)
-      assert [translated_book] == TranslatedBook.all()
+    test "when valid attributes are provided, is valid" do
+      assert changeset(@valid_attrs).valid?
     end
 
-    test "when there is a translated_book with the provided authors and original_book, returns the pre-existent one" do
-      entity_fixture(@valid_attrs)
-      [translated_book] = TranslatedBook.all()
-      same_translated_book = TranslatedBook.maybe_insert!(@valid_attrs)
-      assert translated_book == same_translated_book
+    test "when authors is missing, is invalid" do
+      refute changeset(Map.delete(@valid_attrs, "authors")).valid?
+    end
+
+    test "when authors is nil, is invalid" do
+      refute change_valid(%{"authors" => nil}).valid?
+    end
+
+    test "when authors is empty, is invalid" do
+      refute change_valid(%{"authors" => []}).valid?
+    end
+
+    test "when an author is invalid, is invalid" do
+      refute change_valid(%{"authors" => [%{"name" => nil}]}).valid?
+    end
+
+    test "when original book is missing, is invalid" do
+      refute changeset(Map.delete(@valid_attrs, "original_book")).valid?
+    end
+
+    test "when original book is invalid, is invalid" do
+      refute change_valid(%{"original_book" => %{"title" => nil}}).valid?
+    end
+
+    test "when original book is nil, is invalid" do
+      refute change_valid(%{"original_book" => nil}).valid?
+    end
+
+    test "when a translated book with the provided attributes already exists, is invalid" do
+      {:ok, _} = insert(@valid_attrs)
+      {:error, changeset} = insert(@valid_attrs)
+
+      refute changeset.valid?
+      assert %{authors_fingerprint: :unique} == Repo.get_errors(changeset)
+    end
+  end
+
+  describe "maybe_insert/1" do
+    test "when there is no translated book with the provided authors and original book, inserts it" do
+      assert 0 == count()
+
+      translated_book = TranslatedBook.maybe_insert!(@valid_attrs)
+
+      assert [translated_book] == TranslatedBook.all()
+      assert 1 == count()
+    end
+
+    test "when there is a translated book with the provided authors and original book, returns the pre-existent one" do
+      insert(@valid_attrs)
+      assert 1 == count()
+
+      translated_book = TranslatedBook.maybe_insert!(@valid_attrs) |> TranslatedBook.preload()
+
+      assert [translated_book] == TranslatedBook.all()
+      assert 1 == count()
+    end
+  end
+
+  describe "to_map/1" do
+    test "returns the selected attributes as a map with atom keys" do
+      {:ok, translated_book} = insert(@valid_attrs)
+
+      assert @valid_attrs_atom_keys == TranslatedBook.to_map(translated_book)
     end
   end
 end
