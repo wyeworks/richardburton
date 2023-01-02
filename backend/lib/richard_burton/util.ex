d5541e784dcf3d47b143e8526b5d649f748888a8
Andrés Vidal <andres@wyeworks.com>
2023-01-02 12:56:01 -0300

    Create Util module

---
+++ backend/lib/richard_burton/util.ex
@@ -0,0 +1,30 @@
+defmodule RichardBurton.Util do
+  @moduledoc """
+  Utilities for general purpose
+  """
+
+  def deep_merge_maps(map1, map2) do
+    Map.merge(map1, map2, &deep_merge_resolve/3)
+  end
+
+  defp deep_merge_resolve(_, left = %{}, right = %{}) do
+    deep_merge_maps(left, right)
+  end
+
+  defp deep_merge_resolve(_, _, right) do
+    right
+  end
+
+  def is_value_blank({_, v}) do
+    v == ""
+  end
+
+  def stringify_keys(map) when is_map(map) do
+    Map.new(map, fn {k, v} -> {Atom.to_string(k), stringify_keys(v)} end)
+  end
+
+  def stringify_keys(v) when not is_map(v) do
+    v
+  end
+
+end
