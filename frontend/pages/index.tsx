adadaf9904af5ec99ac5e028843081c322b34487
Andrés Vidal <andres@wyeworks.com>
2023-01-05 11:23:59 -0300

    Wire search in frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,19 +1,27 @@
 import { NextPage } from "next";
+import { useEffect } from "react";
 import { Publication } from "modules/publications";
-import PublicationToolbar from "components/PublicationToolbar";
 import Header from "components/Header";
 import Layout from "components/Layout";
-import { useEffect } from "react";
+import PublicationToolbar from "components/PublicationToolbar";
 import PublicationIndex from "components/PublicationIndex";
+import PublicationSearch from "components/PublicationSearch";
+import { useRouter } from "next/router";
+import { isString } from "lodash";
 
 const Home: NextPage = () => {
   const index = Publication.REMOTE.useIndex();
   const reset = Publication.STORE.useResetAll();
+  const router = useRouter();
+
+  useEffect(() => reset(), [reset]);
 
   useEffect(() => {
-    reset();
-    index();
-  }, [reset, index]);
+    const { search } = router.query;
+    if (router.isReady) {
+      index({ search: isString(search) ? search : undefined });
+    }
+  }, [reset, index, router]);
 
   return (
     <Layout
@@ -21,6 +29,7 @@ const Home: NextPage = () => {
       header={<Header />}
       sidebar={<PublicationToolbar filter nav />}
       content={<PublicationIndex />}
+      subheader={<PublicationSearch />}
     />
   );
 };
