7cc05951568a86d85f51eb8ffc81395f0d55efd7
Andrés Vidal <andres@wyeworks.com>
2023-01-10 02:19:34 -0300

    Access author names in changeset via atom key

--- backend/lib/richard_burton/original_book.ex
+++ backend/lib/richard_burton/original_book.ex
@@ -43,7 +43,7 @@ defmodule RichardBurton.OriginalBook do
 
       authors_fingerprint =
         authors_attrs
-        |> Enum.map_join(&Map.get(&1, "name"))
+        |> Enum.map_join(&Map.get(&1, :name))
         |> Util.create_fingerprint()
 
       # Compute complete changeset with the complete authors associated
