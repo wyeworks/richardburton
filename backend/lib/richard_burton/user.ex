c72c29a6a8ecf9e3ed1c428a2e7e04f0a60e7bff
Andrés Vidal <andres@wyeworks.com>
2023-01-18 14:48:39 -0300

    Implement User.insert/1

--- backend/lib/richard_burton/user.ex
+++ backend/lib/richard_burton/user.ex
@@ -5,6 +5,10 @@ defmodule RichardBurton.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias RichardBurton.User
+  alias RichardBurton.Repo
+  alias RichardBurton.Validation
+
   schema "users" do
     field :subject_id, :string
     field :email, :string
@@ -20,5 +24,16 @@ defmodule RichardBurton.User do
     |> validate_required([:subject_id, :email])
     |> put_change(:role, :reader)
     |> unique_constraint([:subject_id])
+  end
+
+  def all do
+    Repo.all(User)
+  end
+
+  def insert(attrs) do
+    case %User{} |> changeset(attrs) |> Repo.insert() do
+      {:ok, user} -> {:ok, user}
+      {:error, changeset} -> {:error, Validation.get_errors(changeset)}
+    end
   end
 end
