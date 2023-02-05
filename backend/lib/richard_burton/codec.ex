ae66976e66f3f7a7debc75d7fb8d111d03a1fabc
Andrés Vidal <andres@wyeworks.com>
2023-02-05 14:18:05 -0300

    Implement generic nest function in Codec

--- backend/lib/richard_burton/codec.ex
+++ backend/lib/richard_burton/codec.ex
@@ -1,4 +1,6 @@
 defmodule RichardBurton.Codec do
+  alias RichardBurton.Util
+
   @moduledoc """
   Utilities for serialization and deserialization
   """
@@ -51,5 +53,69 @@ defmodule RichardBurton.Codec do
 
   defp compose_keys(key1, key2) when is_binary(key1) and is_binary(key2) do
     "#{key1}_#{key2}"
+  end
+
+  @doc ~S"""
+    Automatically nests a flat map.
+    Keys are grouped by their Longest Common Prefix (LCP) to define nesting points.
+    LCP is calculated considering atomic words separated by underscore:
+
+      `aBC` and `aDE do not have a common prefix
+      `a_BC` and `a_DE` have "a" as common prefix
+      `a_b_BC` and `a_b_DE` have "a_b" as common prefix
+
+    Returns the nested map with string keys.
+
+    ## Examples
+
+        iex> RichardBurton.Codec.nest(%{})
+        %{}
+
+        iex> RichardBurton.Codec.nest(%{a: 1, b: 2, c: 3})
+        %{"a" => 1, "b" => 2, "c" => 3}
+
+        iex> RichardBurton.Codec.nest(%{a: 1, b_a: 1, b_b: 2, b_c: 3})
+        %{"a" => 1, "b" => %{"a" => 1, "b" => 2, "c" => 3}}
+
+        iex> RichardBurton.Codec.nest(%{a: 1, b_a: 1, b_b: 2, b_c_c: 3})
+        %{"a" => 1, "b" => %{"a" => 1, "b" => 2, "c_c" => 3}}
+
+        iex> RichardBurton.Codec.nest(%{a: 1, b_a: 1, b_b: 2, b_c_c_c: 3})
+        %{"a" => 1, "b" => %{"a" => 1, "b" => 2, "c_c_c" => 3}}
+
+        iex> RichardBurton.Codec.nest(%{a: 1, b_a: 1, b_b: 2, b_c_c: 3, b_c_d: 4})
+        %{"a" => 1, "b" => %{"a" => 1, "b" => 2, "c" => %{"c" => 3, "d" => 4}}}
+  """
+  def nest(map) do
+    map
+    |> Util.stringify_keys()
+    |> Enum.map(fn {k, v} -> {String.split(k, "_"), v} end)
+    |> Enum.reduce(%{}, fn {path, v}, acc -> put_path(acc, path, v) end)
+    |> Map.new(&coalesce_keys/1)
+  end
+
+  defp put_path(map, [key], value) do
+    Map.put(map, key, value)
+  end
+
+  defp put_path(map, [key | rest], value) do
+    Map.put(map, key, put_path(map[key] || %{}, rest, value))
+  end
+
+  defp coalesce_keys({key, map}) when is_map(map) do
+    case(coalesce_keys(map)) do
+      {inner_key, v} -> {compose_keys(key, inner_key), v}
+      v -> {key, v}
+    end
+  end
+
+  defp coalesce_keys({key, value}), do: {key, value}
+
+  defp coalesce_keys(map) when is_map(map) do
+    case Map.to_list(map) do
+      [{k, m}] when is_map(m) -> coalesce_keys({k, m})
+      [{k, v}] -> {k, v}
+      _ -> Map.new(map, &coalesce_keys/1)
+    end
   end
 end
