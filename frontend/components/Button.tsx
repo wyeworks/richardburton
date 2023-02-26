478316465126e5f80e09bff2a4a887c864a7a4e0
Andrés Vidal <andres@wyeworks.com>
2023-02-26 11:44:28 -0300

    Replace index sidebar with footer

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -1,4 +1,4 @@
-import classNames from "classnames";
+import c from "classnames";
 import { FC, forwardRef, HTMLProps } from "react";
 
 type Props = HTMLProps<HTMLButtonElement> & {
@@ -9,7 +9,15 @@ type Props = HTMLProps<HTMLButtonElement> & {
 };
 
 const Button = forwardRef<HTMLButtonElement, Props>(function Button(
-  { label, onClick, type = "primary", Icon, alignment = "center", ...props },
+  {
+    label,
+    onClick,
+    type = "primary",
+    Icon,
+    alignment = "center",
+    className,
+    ...props
+  },
   ref
 ) {
   const isPrimary = type === "primary";
@@ -20,18 +28,21 @@ const Button = forwardRef<HTMLButtonElement, Props>(function Button(
     <button
       {...props}
       ref={ref}
-      className={classNames(
+      className={c(
         "w-full flex px-2 py-1.5 transition-colors items-center rounded font-base shadow-sm text-xs",
         "disabled:bg-slate-300",
         {
           "text-white bg-indigo-600 hover:bg-indigo-700": isPrimary,
           "bg-gray-100 hover:bg-white/50": isOutline,
           "justify-center": isTextCentered,
-        }
+        },
+        className
       )}
       onClick={onClick}
     >
-      {Icon && <Icon className="w-4 h-4 mr-2 text-white" />}
+      {Icon && (
+        <Icon className={c("w-5 h-5 mr-2", { "text-indigo-700": isOutline })} />
+      )}
       {label}
     </button>
   );
