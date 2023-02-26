813e823370d30e1b71aa30b3407b84574ae0c03a
Andrés Vidal <andres@wyeworks.com>
2023-02-26 20:00:52 -0300

    Tweak /publications/new UI

---
+++ components/PublicationCounter.tsx
@@ -0,0 +1,24 @@
+import { Publication } from "modules/publications";
+import { FC } from "react";
+import SparklesIcon from "assets/sparkles.svg";
+import Tooltip from "./Tooltip";
+
+const PublicationCounter: FC = () => {
+  const publicationCount = Publication.STORE.useVisibleCount();
+
+  return publicationCount !== 0 ? (
+    <Tooltip
+      info
+      message={`${publicationCount} new ${
+        publicationCount === 1 ? "publication" : "publications"
+      }`}
+    >
+      <div className="flex items-center px-2 space-x-2 text-xs rounded shadow-sm">
+        <SparklesIcon className="w-4 h-4 text-indigo-600" />
+        <span>{publicationCount} </span>
+      </div>
+    </Tooltip>
+  ) : null;
+};
+
+export default PublicationCounter;
---
+++ components/PublicationErrorCounter.tsx
@@ -0,0 +1,26 @@
+import { Publication } from "modules/publications";
+import { FC } from "react";
+import ErrorCircleIcon from "assets/error-circle.svg";
+import Tooltip from "./Tooltip";
+
+const PublicationErrorCounter: FC = () => {
+  const publicationCount = Publication.STORE.useVisibleCount();
+  const validPublicationCount = Publication.STORE.useValidCount();
+  const invalidPublicationCount = publicationCount - validPublicationCount;
+
+  return invalidPublicationCount !== 0 ? (
+    <Tooltip
+      error
+      message={`${invalidPublicationCount} ${
+        publicationCount === 1 ? "publication" : "publications"
+      } with errors`}
+    >
+      <div className="flex items-center px-2 space-x-2 text-xs text-white bg-red-500 rounded shadow-sm">
+        <ErrorCircleIcon className="w-4 h-4" />
+        <span>{invalidPublicationCount} </span>
+      </div>
+    </Tooltip>
+  ) : null;
+};
+
+export default PublicationErrorCounter;
