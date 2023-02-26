a3b52d02709c40b34ef07ba250754e38fd51beb6
Andrés Vidal <andres@wyeworks.com>
2023-02-26 19:38:40 -0300

    Remove PublicationToolbar from new page

---
+++ components/ResetOverridden.tsx
@@ -0,0 +1,35 @@
+import { Publication } from "modules/publications";
+import { FC } from "react";
+import Button from "./Button";
+import RestorePageIcon from "assets/restore-page.svg";
+import { useClearSelection } from "react-selection-manager";
+
+const ResetOverridden: FC = () => {
+  const overriddenCount = Publication.STORE.useOverriddenCount();
+  const resetOverridden = Publication.STORE.useResetOverridden();
+  const clearSelection = useClearSelection();
+
+  const overriddenIds = Publication.STORE.useOverriddenIds();
+  const validate = Publication.REMOTE.useValidate();
+
+  const reset = () => {
+    if (!overriddenIds)
+      throw "Can not reset publications: overriden ids are undefined.";
+    resetOverridden();
+    clearSelection();
+    validate(overriddenIds);
+  };
+
+  return overriddenCount !== 0 ? (
+    <Button
+      label={`Reset ${overriddenCount} overriden`}
+      type="outline"
+      Icon={RestorePageIcon}
+      alignment="left"
+      width="fixed"
+      onClick={reset}
+    />
+  ) : null;
+};
+
+export default ResetOverridden;
--- components/ToolbarHeading.tsx
+++ components/ToolbarHeading.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const ToolbarHeading: FC<{ label: string }> = ({ label }) => (
-  <h3 className="flex items-center space-x-2 text-sm">
+  <h3 className="flex items-center my-4 space-x-2 text-sm">
     <span className="border-b grow h-fit" />
     <span className="text-gray-500">{label}</span>
     <span className="border-b grow h-fit" />
