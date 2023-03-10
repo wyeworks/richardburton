f7863778c3d53f1fd88cb72c6eafab7dc754d61d
Andrés Vidal <andres@wyeworks.com>
2023-03-09 21:52:53 -0300

    Implement basic Select

--- frontend/components/MenuItem.tsx
+++ frontend/components/MenuItem.tsx
@@ -1,22 +1,34 @@
-import { DetailedHTMLProps, forwardRef, LiHTMLAttributes } from "react";
+import {
+  DetailedHTMLProps,
+  forwardRef,
+  LiHTMLAttributes,
+  MouseEventHandler,
+} from "react";
 import c from "classnames";
 
 type Props = Omit<
   DetailedHTMLProps<LiHTMLAttributes<HTMLLIElement>, HTMLLIElement>,
-  "className"
+  "className" | "onClick"
 > & {
   selected: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
 export default forwardRef<HTMLLIElement, Props>(function MenuItem(
-  { selected, ...props },
+  { selected, children, onClick, ...props },
   ref
 ) {
   return (
     <li
-      className={c("px-2.5 py-1", { "bg-indigo-100": selected })}
+      className={c("px-2.5 py-1 focus:bg-indigo-100", {
+        "bg-indigo-100": selected,
+      })}
       ref={ref}
       {...props}
-    />
+    >
+      <button className="w-full text-left" onClick={onClick}>
+        {children}
+      </button>
+    </li>
   );
 });
