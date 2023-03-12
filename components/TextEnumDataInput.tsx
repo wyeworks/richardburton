cea59975e796e02c8fd5862e19ba1be826040b30
Andrés Vidal <andres@wyeworks.com>
2023-03-11 21:02:27 -0300

    Move validate and override logic to DataInput

--- components/TextEnumDataInput.tsx
+++ components/TextEnumDataInput.tsx
@@ -1,10 +1,9 @@
-import { FC, ForwardedRef, forwardRef, useEffect, useState } from "react";
+import { FC, forwardRef } from "react";
 import { Publication } from "modules/publications";
 import { DataInputProps } from "./DataInput";
+import Select, { SelectOption } from "./Select";
 import countries from "i18n-iso-countries";
 import countriesEN from "i18n-iso-countries/langs/en.json";
-import c from "classnames";
-import Select, { SelectOption } from "./Select";
 
 //TODO: decouple this component from countries
 countries.registerLocale(countriesEN);
@@ -15,46 +14,24 @@ const OPTIONS = Object.entries(COUNTRIES).map(([key, label]) => ({
   label: label,
 }));
 
-export default forwardRef<HTMLElement, DataInputProps>(function TextDataInput(
-  { rowId, colId, error, value: data, autoValidated, onChange, ...props },
-  ref
-) {
-  const validate = Publication.REMOTE.useValidate();
-  const override = Publication.STORE.ATTRIBUTES.useOverride();
-  const [value, setValue] = useState(data);
+export default forwardRef<HTMLInputElement, DataInputProps>(
+  function TextDataInput({ rowId, colId, onChange, ...props }, ref) {
+    const placeholder = Publication.ATTRIBUTE_LABELS[colId];
+    const error = Boolean(props.error);
 
-  function doValidate() {
-    if (autoValidated) {
-      validate([rowId]);
+    function handleChange(option: SelectOption) {
+      onChange?.(option.id);
     }
+
+    return (
+      <Select
+        {...props}
+        ref={ref}
+        placeholder={placeholder}
+        onChange={handleChange}
+        error={error}
+        options={OPTIONS}
+      />
+    );
   }
-
-  function handleChange(option: SelectOption) {
-    setValue(option.label);
-    override(rowId, colId, option.id);
-    onChange?.(option.id);
-  }
-
-  useEffect(() => {
-    if (COUNTRIES[data] !== value) {
-      setValue(COUNTRIES[data]);
-    }
-  }, [data, rowId, colId, value, setValue]);
-
-  return (
-    <Select
-      {...props}
-      ref={ref as ForwardedRef<HTMLInputElement>}
-      placeholder={Publication.ATTRIBUTE_LABELS[colId]}
-      className={c(
-        "px-2 py-1 rounded outline-none bg-transparent focus:bg-gray-active focus:shadow-sm placeholder:text-xs",
-        "error:focus:bg-red-400/80 error:bg-red-300/40 error:focus:text-white error:shadow-sm error:placeholder-white"
-      )}
-      value={value}
-      onBlur={doValidate}
-      onChange={handleChange}
-      error={Boolean(error)}
-      options={OPTIONS}
-    />
-  );
-}) as FC<DataInputProps>;
+) as FC<DataInputProps>;
