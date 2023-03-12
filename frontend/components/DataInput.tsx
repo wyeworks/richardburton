6f6db5acf98e20fcdb5b9721e2f48b0ceedfecaa
Andrés Vidal <andres@wyeworks.com>
2023-03-11 21:02:27 -0300

    Move validate and override logic to DataInput

--- frontend/components/DataInput.tsx
+++ frontend/components/DataInput.tsx
@@ -1,4 +1,4 @@
-import { FC, forwardRef, HTMLProps, Ref } from "react";
+import { FC, forwardRef, HTMLProps, Ref, useEffect, useState } from "react";
 import TextDataInput from "./TextDataInput";
 import TextArrayDataInput from "./TextArrayDataInput";
 import {
@@ -9,6 +9,12 @@ import {
 } from "modules/publications";
 import Tooltip from "./Tooltip";
 import TextEnumDataInput from "./TextEnumDataInput";
+import countries from "i18n-iso-countries";
+import countriesEN from "i18n-iso-countries/langs/en.json";
+
+//TODO: decouple this component from countries
+countries.registerLocale(countriesEN);
+const COUNTRIES = countries.getNames("en", { select: "official" });
 
 const COMPONENTS_PER_TYPE: Record<PublicationKeyType, FC<Props>> = {
   text: TextDataInput,
@@ -31,14 +37,53 @@ const DataInput = forwardRef<HTMLElement, Props>(function DataInput(
   props,
   ref
 ) {
+  const { rowId, colId, value: data, autoValidated, onChange } = props;
+
   const type = Publication.ATTRIBUTE_TYPES[props.colId];
   const Component = COMPONENTS_PER_TYPE[type];
+
+  const validate = Publication.REMOTE.useValidate();
+  const override = Publication.STORE.ATTRIBUTES.useOverride();
+  const [value, setValue] = useState(data);
+
+  function doValidate() {
+    if (autoValidated) {
+      validate([rowId]);
+    }
+  }
+
+  function handleChange(value: string) {
+    console.log(value);
+    setValue(value);
+    override(rowId, colId, value);
+    onChange?.(value);
+  }
+
+  useEffect(() => {
+    if (colId === "country") {
+      if (COUNTRIES[data] !== value) {
+        setValue(COUNTRIES[data]);
+      }
+    } else {
+      if (data !== value) {
+        setValue(data);
+      }
+    }
+  }, [data, rowId, colId, value, setValue]);
+
   return (
     <Tooltip error message={props.error}>
-      <Component {...props} ref={ref} />
+      <Component
+        {...props}
+        ref={ref}
+        value={value}
+        onChange={handleChange}
+        onBlur={doValidate}
+      />
     </Tooltip>
   );
 });
 
+export { COUNTRIES };
 export type { Props as DataInputProps };
 export default DataInput;
--- frontend/components/TextArrayDataInput.tsx
+++ frontend/components/TextArrayDataInput.tsx
@@ -1,59 +1,26 @@
-import {
-  FC,
-  ForwardedRef,
-  forwardRef,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import { FC, forwardRef, useMemo } from "react";
 import { Publication } from "modules/publications";
 import { Author } from "modules/authors";
 import { DataInputProps } from "./DataInput";
 import Multicombobox from "./Multicombobox";
 import useDebounce from "utils/useDebounce";
 
-export default forwardRef<HTMLElement, DataInputProps>(
+export default forwardRef<HTMLDivElement, DataInputProps>(
   function TextArrayDataInput(
-    {
-      rowId,
-      colId,
-      value: data,
-      error,
-      autoValidated,
-      onChange,
-      onBlur,
-      ...props
-    },
+    { rowId, colId, value, onChange, onBlur, ...props },
     ref
   ) {
-    const validate = Publication.REMOTE.useValidate();
-    const override = Publication.STORE.ATTRIBUTES.useOverride();
-    const [value, setValue] = useState(data);
+    const placeholder = Publication.ATTRIBUTE_LABELS[colId];
+    const error = Boolean(props.error);
 
     const items = useMemo(
       () => (value === "" ? [] : value.split(",")),
       [value]
     );
 
-    function doValidate() {
-      if (autoValidated) {
-        validate([rowId]);
-      }
-    }
-
     function handleChange(value: string[]) {
-      const v = value.join(",");
-      setValue(v);
-      override(rowId, colId, v);
-      doValidate();
-      onChange?.(v);
+      onChange?.(value.join(","));
     }
-
-    useEffect(() => {
-      if (data !== value) {
-        setValue(data);
-      }
-    }, [data, rowId, colId, value, setValue]);
 
     //TODO: decouple this component from author search
     const getOptions = useDebounce(Author.REMOTE.search, 350, {
@@ -63,12 +30,11 @@ export default forwardRef<HTMLElement, DataInputProps>(
     return (
       <Multicombobox
         {...props}
-        ref={ref as ForwardedRef<HTMLDivElement>}
-        placeholder={Publication.ATTRIBUTE_LABELS[colId]}
+        ref={ref}
+        placeholder={placeholder}
         value={items}
-        onBlur={doValidate}
+        error={error}
         onChange={handleChange}
-        error={Boolean(error)}
         getOptions={getOptions}
       />
     );
