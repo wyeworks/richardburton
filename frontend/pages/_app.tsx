6c97976fae36fb04e0f362049474defc76f8c082
Andrés Vidal <andres@wyeworks.com>
2023-03-12 18:21:30 -0300

    Enhance error reporting

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -34,12 +34,26 @@ function request<T = void>(
     } catch (error) {
       if (axios.isAxiosError(error)) {
         if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
           if (error.response.status === 409) {
             reject("conflict");
           } else {
-            reject((error.response?.data as any).errors?.detail);
+            const detail = (error.response?.data as any).errors?.detail;
+
+            if (detail) {
+              reject(detail);
+            } else {
+              reject(error.response?.data);
+            }
           }
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          reject(error.message);
         } else {
+          // Something happened in setting up the request that triggered an Error
           reject(error.message);
         }
       } else {
