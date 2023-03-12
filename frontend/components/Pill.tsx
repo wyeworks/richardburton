86d0040b76023fbdf69ce0d52094f0537bf132de
Andrés Vidal <andres@wyeworks.com>
2023-03-12 17:27:57 -0300

    Implement and wire NumberInput

--- frontend/components/Pill.tsx
+++ frontend/components/Pill.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import CloseIcon from "assets/close.svg";
+import c from "classnames";
 
 type Props = {
   label: string;
@@ -11,7 +12,11 @@ const Pill: FC<Props> = ({ label, onRemove }) => {
     <span className="flex items-center space-x-1 shrink-0 px-1 py-0.5 border border-indigo-600 text-indigo-600 rounded">
       <span>{label}</span>
       <button
-        className="inline-flex items-center justify-center transition-colors rounded-full hover:bg-indigo-500 hover:text-white"
+        className={c(
+          "inline-flex items-center justify-center transition-colors rounded-full",
+          "hover:bg-indigo-500 hover:text-white",
+          "focus:bg-indigo-500 focus:text-white outline-none"
+        )}
         onClick={onRemove}
       >
         <CloseIcon className="inline w-3 aspect-square" />
