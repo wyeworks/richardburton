5cadeca48a5bd3e48476e2d8d1ae1d76c22ed3eb
Andrés Vidal <andres@wyeworks.com>
2023-05-16 01:01:11 -0300

    Remove tooltip from PublicationDeselect

--- frontend/components/PublicationDeselect.tsx
+++ frontend/components/PublicationDeselect.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import DeselectIcon from "assets/deselect.svg";
-import Tooltip from "./Tooltip";
 import { useSelectionSize, useClearSelection } from "react-selection-manager";
 import Button from "./Button";
 
@@ -8,20 +7,14 @@ const PublicationDeselect: FC = () => {
   const selectionSize = useSelectionSize();
   const clearSelection = useClearSelection();
 
-  const message = `${selectionSize} ${
-    selectionSize === 1 ? "publication" : "publications"
-  } selected`;
-
   return (
-    <Tooltip info message={message}>
-      <Button
-        type="outline"
-        width="fit"
-        label={`Deselect ${selectionSize}`}
-        Icon={DeselectIcon}
-        onClick={clearSelection}
-      />
-    </Tooltip>
+    <Button
+      type="outline"
+      width="fit"
+      label={`Deselect ${selectionSize}`}
+      Icon={DeselectIcon}
+      onClick={clearSelection}
+    />
   );
 };
 
