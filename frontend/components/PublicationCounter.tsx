422f74490f88abb1c39136b61bdec2fa3309236d
Andrés Vidal <andres@wyeworks.com>
2023-05-17 00:38:04 -0300

    Use Button in PublicationCounter for consistency
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- frontend/components/PublicationCounter.tsx
+++ frontend/components/PublicationCounter.tsx
@@ -2,6 +2,7 @@ import { Publication } from "modules/publication";
 import { FC } from "react";
 import SparklesIcon from "assets/sparkles.svg";
 import Tooltip from "./Tooltip";
+import Button from "./Button";
 
 const PublicationCounter: FC = () => {
   const publicationCount = Publication.STORE.useVisibleCount();
@@ -12,10 +13,12 @@ const PublicationCounter: FC = () => {
 
   return publicationCount !== 0 ? (
     <Tooltip info message={message}>
-      <div className="flex items-center px-2 space-x-2 text-xs rounded shadow-sm select-none hover:bg-gray-active">
-        <SparklesIcon className="w-4 h-4 text-indigo-600" />
-        <span>{publicationCount} </span>
-      </div>
+      <Button
+        type="outline"
+        width="fit"
+        Icon={SparklesIcon}
+        label={`${publicationCount}`}
+      />
     </Tooltip>
   ) : null;
 };
