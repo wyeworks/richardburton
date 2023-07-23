30f3ff2b4922ea3aa7fc28b6901a24846bcaecc5
Andrés Vidal <andres@wyeworks.com>
2023-07-23 11:55:06 -0300

    Animate hidden attributes

--- frontend/components/PublicationHiddenAttributes.tsx
+++ frontend/components/PublicationHiddenAttributes.tsx
@@ -1,28 +1,37 @@
 import { Publication } from "modules/publication";
 import { FC } from "react";
 import c from "classnames";
+import { motion, AnimatePresence } from "framer-motion";
 
 const PublicationHiddenAttributes: FC = () => {
   const hiddenAttributes = Publication.STORE.ATTRIBUTES.useHidden();
 
   return (
     <ol className="flex h-full shadow">
-      {hiddenAttributes.map((key) => (
-        <li
-          key={key}
-          className={c(
-            "relative origin-top-left w-8 shadow text-xs first:rounded-l last:rounded-r",
-            "text-white bg-indigo-600"
-          )}
-        >
-          <div
-            className="absolute z-20 text-center rotate-180 translate-x-1/2 top-3 whitespace-nowrap"
-            style={{ writingMode: "vertical-lr" }}
+      <AnimatePresence>
+        {hiddenAttributes.map((key) => (
+          <motion.li
+            key={key}
+            className={c(
+              "relative origin-top-left shadow text-xs first:rounded-l last:rounded-r",
+              "text-white bg-indigo-600"
+            )}
+            initial={{ width: 0 }}
+            animate={{ width: "2rem" }}
+            exit={{ width: 0 }}
           >
-            {Publication.ATTRIBUTE_LABELS[key]}
-          </div>
-        </li>
-      ))}
+            <motion.div
+              className="absolute z-20 text-center rotate-180 translate-x-1/2 top-3 whitespace-nowrap"
+              style={{ writingMode: "vertical-lr" }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              {Publication.ATTRIBUTE_LABELS[key]}
+            </motion.div>
+          </motion.li>
+        ))}
+      </AnimatePresence>
     </ol>
   );
 };
