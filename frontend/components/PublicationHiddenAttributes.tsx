b4e1ef9af8b243012769407c14660ffe9a98cad0
Andrés Vidal <andres@wyeworks.com>
2023-07-23 12:51:53 -0300

    Add button to make show columns from aside

--- frontend/components/PublicationHiddenAttributes.tsx
+++ frontend/components/PublicationHiddenAttributes.tsx
@@ -2,35 +2,53 @@ import { Publication } from "modules/publication";
 import { FC } from "react";
 import c from "classnames";
 import { motion, AnimatePresence } from "framer-motion";
+import VisibilityOnIcon from "assets/visibility-on.svg";
+import Tooltip from "./Tooltip";
 
 const PublicationHiddenAttributes: FC = () => {
   const hiddenAttributes = Publication.STORE.ATTRIBUTES.useHidden();
+  const setVisible = Publication.STORE.ATTRIBUTES.useSetVisible();
 
   return (
     <ol className="flex h-full shadow">
       <AnimatePresence>
-        {hiddenAttributes.map((key) => (
-          <motion.li
-            key={key}
-            className={c(
-              "relative origin-top-left overflow-clip shadow text-xs first:rounded-l last:rounded-r",
-              "text-white bg-indigo-600"
-            )}
-            initial={{ width: 0 }}
-            animate={{ width: "2rem" }}
-            exit={{ width: 0 }}
-          >
-            <motion.div
-              className="absolute z-20 text-center rotate-180 translate-x-1/2 top-3 whitespace-nowrap"
-              style={{ writingMode: "vertical-lr" }}
-              initial={{ left: "-2rem" }}
-              animate={{ left: 0 }}
-              exit={{ left: "-2rem" }}
+        {hiddenAttributes.map((key) => {
+          const label = `Show ${Publication.ATTRIBUTE_LABELS[key]}`;
+          return (
+            <motion.li
+              key={key}
+              className={c(
+                "relative origin-top-left overflow-clip shadow text-xs first:rounded-l last:rounded-r",
+                "text-white bg-indigo-600 hover:bg-indigo-700 transition-colors "
+              )}
+              initial={{ width: 0 }}
+              animate={{ width: "2rem" }}
+              exit={{ width: 0 }}
             >
-              {Publication.ATTRIBUTE_LABELS[key]}
-            </motion.div>
-          </motion.li>
-        ))}
+              <Tooltip message={label} info>
+                <button
+                  className="flex items-start justify-center w-full h-full p-1 group"
+                  onClick={() => setVisible([key])}
+                  aria-label={label}
+                >
+                  <VisibilityOnIcon
+                    role="presentation"
+                    className="invisible w-5 h-5 group-hover:visible"
+                  />
+                  <motion.div
+                    className="absolute z-20 text-center rotate-180 translate-x-1/2 top-10 whitespace-nowrap"
+                    style={{ writingMode: "vertical-lr" }}
+                    initial={{ left: "-2rem" }}
+                    animate={{ left: 0 }}
+                    exit={{ left: "-2rem" }}
+                  >
+                    {Publication.ATTRIBUTE_LABELS[key]}
+                  </motion.div>
+                </button>
+              </Tooltip>
+            </motion.li>
+          );
+        })}
       </AnimatePresence>
     </ol>
   );
