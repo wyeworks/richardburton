9a13e90712c638038c27e6c1e15696b582314007
Andrés Vidal <andres@wyeworks.com>
2024-01-21 13:40:32 -0300

    Tweak open graph configuration
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,8 +1,11 @@
-import { SUBHEADING_TEXT } from "components/Layout";
 import { DocumentProps, Head, Html, Main, NextScript } from "next/document";
 import { FC } from "react";
 
-const DESCRIPTION = SUBHEADING_TEXT;
+const IMAGE_ALT =
+  "Richard Burton Platform: A database about Brazilian literature in translation";
+const DESCRIPTION =
+  "The Richard Burton Platform is an open access online repository with reliable data about Brazilian literature in translation to English, stemmed from the need to preserve the Brazilian cultural and historical heritage as well as its international dissemination.";
+
 const KEYWORDS = "Brazilian, Literature, Richard Burton, Translation, English";
 
 const Document: FC<DocumentProps> = () => {
@@ -10,7 +13,7 @@ const Document: FC<DocumentProps> = () => {
     <Html className="h-screen">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="description" content={SUBHEADING_TEXT} />
+        <meta name="description" content={DESCRIPTION} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Andrés Vidal" />
         <meta name="keywords" content={KEYWORDS} />
@@ -18,10 +21,16 @@ const Document: FC<DocumentProps> = () => {
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content="Richard Burton Platform" />
         <meta property="og:description" content={DESCRIPTION} />
-        <meta property="og:image" content="/thumbnail.svg" />
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:image:alt" content={IMAGE_ALT} />
         <meta property="og:type" content="website" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="627" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image:alt" content={IMAGE_ALT} />
+        <meta name="twitter:title" content="Richard Burton Platform" />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta name="twitter:image" content="/thumbnail.png" />
       </Head>
       <body>
         <Main />
