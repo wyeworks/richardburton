5747476a2c80ff6efa8446edc4b94d1e61b607f6
Andrés Vidal <andres@wyeworks.com>
2024-01-22 01:53:24 -0300

    Fix test
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
+import magicalSvg from "vite-plugin-magical-svg";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), magicalSvg({ target: "react" })],
   test: {
     environment: "jsdom",
     globals: true,
