6bf50d4c92f68b995a56d3908fab2f0b92210aa3
Andrés Vidal <andres@wyeworks.com>
2024-04-22 23:53:39 -0300

    Reintroduce page title
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -12,6 +12,7 @@ const Document: FC<DocumentProps> = () => {
   return (
     <Html className="h-screen">
       <Head>
+        <title>Richard Burton</title>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="description" content={DESCRIPTION} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
