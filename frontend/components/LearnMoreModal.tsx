d4e90aa996016d03261b320af4e1191a6c7b37a1
Andrés Vidal <andres@wyeworks.com>
2024-06-05 00:03:17 -0300

    Update the LearnMoreModal content
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- frontend/components/LearnMoreModal.tsx
+++ frontend/components/LearnMoreModal.tsx
@@ -14,7 +14,7 @@ const AboutRichardBurton: FC = () => {
       <header className="sticky z-30 px-1 py-2 bg-white top-10 sm:top-4">
         <h2 className="text-lg">
           About
-          <strong className="font-normal"> Richard Burton</strong>
+          <strong className="font-normal"> Richard & Isabel Burton</strong>
         </h2>
       </header>
       <div className="space-y-4">
@@ -22,26 +22,36 @@ const AboutRichardBurton: FC = () => {
           <Image
             fill
             alt="Portrait of Sir Richard Burton"
-            src="/richard.jpg"
+            src="/iracema.jpeg"
             sizes="(max-width: 300px)"
           />
         </div>
         <p>
-          According to{" "}
-          <a className="anchor" target="__blank" href="https://burtoniana.org">
-            Burtoniana (2024)
+          The platform is named after the first known translators of a book of
+          Brazilian literature into English. Sir Richard Francis Burton
+          (1821-1890), and his wife Isabel Burton (1831-1896) are considered the
+          pioneers in translating Brazilian literature into English. They had
+          contact with author José de Alencar and his literary works while Sir
+          Burton served as British consul in Santos, Brazil (1865-1868). The
+          English translation for{" "}
+          <a
+            className="anchor"
+            target="__blank"
+            href="https://archive.org/details/iramahoneylipsle00alen/mode/2up?view=theater"
+          >
+            Iracema, The Honey-lips: A legend of Brazil
           </a>
-          , Richard Burton was a soldier, explorer, linguist, ethnologist, and
-          controversialist. Mostly self-educated, he mastered half a dozen
-          Eastern languages after joining the Army of the East India Company in
-          1842, and is deemed the first translator of a Brazilian literature
-          book, together with his wife Isabel Burton: &quot;Iracema, The
-          Honey-lips: A legend of Brazil&quot; (1886).
+          (1886), by José de Alencar, was first published by Bickers and Son
+          publishing house, in London. After this first step, hundreds of books
+          of literature have been translated into English with the efforts of
+          independent translators, literary agents, publishing houses, and
+          universities, making Brazilian literature available to the Anglophone
+          readership.
         </p>
 
         <ol className="ml-5 space-y-2 list-disc">
           <li>
-            Read more about Richard Burton&apos;s life at{" "}
+            Read more about Richard & Isabel Burton at{" "}
             <a
               href="https://burtoniana.org"
               target="__blank"
@@ -49,9 +59,18 @@ const AboutRichardBurton: FC = () => {
             >
               burtoniana.org
             </a>
+            and{" "}
+            <a
+              href="http://www.sirrichardburtonmuseum.co.uk"
+              target="__blank"
+              className="anchor"
+            >
+              sir richard burton museum.co.uk
+            </a>
+            .
           </li>
           <li>
-            Browse books translated by Richard Burton{" "}
+            Browse books translated by Richard & Isabel Burton{" "}
             <Link
               href={`/?search=Richard+Burton`}
               className="anchor"
@@ -69,12 +88,17 @@ const AboutRichardBurton: FC = () => {
 const AboutRichardBurtonPlatform: FC = () => (
   <div className="space-y-4">
     <p>
-      The <em className="italic font-normal">Richard Burton Platform</em> is an
-      open access online repository with reliable data about Brazilian
+      The{" "}
+      <em className="italic font-normal">Richard & Isabel Burton Platform</em>{" "}
+      is an open access online repository with reliable data about Brazilian
       literature in translation to English, stemmed from the need to preserve
       the Brazilian cultural and historical heritage as well as its
-      international dissemination. The first version for the platform was
-      registered by the{" "}
+      international dissemination. The platform intends to contribute to
+      translation flows worldwide, facilitating the access of Brazilian
+      literature to Anglophone readers and researchers.
+    </p>
+    <p>
+      The first version of the platform was registered by the{" "}
       <a href="https://ifrs.edu.br/" target="__blank" className="anchor">
         Federal Institute of Education, Science and Technology of Rio Grande do
         Sul (IFRS)
@@ -92,7 +116,7 @@ const AboutRichardBurtonPlatform: FC = () => (
       to Anglophone readers and researchers.
     </p>
     <p>
-      The present version has been developed since 2022, through the research
+      The current version has been developed since 2022, through the research
       project{" "}
       <em className="italic font-normal">
         &quot;Brazilian Literature in Translation: Solutions in Science and
@@ -106,12 +130,13 @@ const AboutRichardBurtonPlatform: FC = () => (
       <a className="anchor" target="__blank" href="https://wyeworks.com">
         WyeWorks
       </a>
-      &apos;s collaboration. Its name is derived from Sir Richard Francis
-      Burton, one of the pioneers in translating Brazilian literature into
-      English. The platform&apos;s data collection includes books from Brazilian
-      literature translated into English. It doesn&apos;t include anthologies,
-      collections or works published in magazines, newspapers and/or electronic
-      media.
+      &apos;s collaboration.{" "}
+    </p>
+    <p>
+      The R&I platform&apos;s data collection includes titles of books from
+      Brazilian literature translated into English. It doesn&apost include
+      titles of anthologies, collections or works published in magazines,
+      newspapers and/or electronic media.
     </p>
   </div>
 );
@@ -119,7 +144,7 @@ const AboutRichardBurtonPlatform: FC = () => (
 const LearnMoreHeading: FC = () => (
   <div className="font-light">
     About the <br className="sm:hidden" />
-    <strong className="font-normal">Richard Burton Platform</strong>
+    <strong className="font-normal">Richard & Isabel Burton Platform</strong>
   </div>
 );
 
